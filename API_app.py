af766cf2880b0bd4ab425b475d64591c68b03d0a
YoussefPro-698 <youssef.pro.coding@gmail.com>
2024-07-18 14:59:03 +0200

    V beta of the API app is completed

--- API_app.py
+++ API_app.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.graphics import Color, Rectangle
 from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
@@ -14,6 +15,18 @@ url = 'http://127.0.0.1:8000/API/IRVRVHNOIUNOUZHNOZIJNC/Get-All-Json-Data'
 class MainWidget(TabbedPanel):
     pass
 
+class CustomBoxLayout(BoxLayout):
+    def __init__(self, bg_color=(0.3, 0.3, 0.3, 1), **kwargs):
+        super().__init__(**kwargs)
+        with self.canvas.before:
+            Color(*bg_color)
+            self.rect = Rectangle(size=self.size, pos=self.pos)
+
+        self.bind(size=self._update_rect, pos=self._update_rect)
+
+    def _update_rect(self, instance, value):
+        self.rect.pos = instance.pos
+        self.rect.size = instance.size
 
 class OrderView(ScrollView):
     def __init__(self, **kwargs):
@@ -28,7 +41,6 @@ class OrderView(ScrollView):
             response = requests.get(url)
             response.raise_for_status()
             data = response.json()
-            print("Data retrieved:", data)  # Debugging line
 
             orders = data.get('order', [])
             if orders:
@@ -50,22 +62,6 @@ class OrderView(ScrollView):
             print("Error loading orders:", e)  # Debugging line
 
 
-class OrderedProductItem(StackLayout):
-    nom = StringProperty()
-    prix = StringProperty()
-
-    def __init__(self, nom, prix, **kwargs):
-        super().__init__(**kwargs)
-        self.orientation = 'lr-tb'
-        self.size_hint = (1, None)
-        self.height = 30
-        self.nom = nom
-        self.prix = str(prix) + " DH"
-
-        self.add_widget(Label(text=self.nom, size_hint=(0.7, None), height=30, halign='right'))
-        self.add_widget(Label(text=self.prix, size_hint=(0.3, None), height=30, halign='right'))
-
-
 class OrderItem(StackLayout):
     numero_de_commande = StringProperty()
     order_name = StringProperty()
@@ -85,11 +81,11 @@ class OrderItem(StackLayout):
         self.height = self.minimum_height
 
         self.numero_de_commande = "#" + str(numero_de_commande)
-        self.order_name = order_name
-        self.order_phone = order_phone
-        self.order_email = order_email
-        self.order_address = order_address
-        self.order_date = order_date
+        self.order_name = str(order_name)
+        self.order_phone = str(order_phone)
+        self.order_email = str(order_email)
+        self.order_address = str(order_address)
+        self.order_date = str(order_date)
         self.totale_str = f"{totale} DH"
 
         self.add_widget(self.create_label_pair("Facture de commande", self.numero_de_commande))
@@ -101,24 +97,26 @@ class OrderItem(StackLayout):
 
         self.add_widget(self.create_label("Produits commandés:"))
         for product in ordered_products:
-            product_name = product['name']
-            product_price = product['price']
-            product_widget = OrderedProductItem(product_name, product_price)
-            self.add_widget(product_widget)
+            product_name = str(product['name'])
+            product_price = str(product['price']) + " DH"
+            self.add_widget(self.create_label_pair(product_name, product_price))
 
-        product_widget = OrderedProductItem("Totale payé", totale)
-        self.add_widget(product_widget)
+        self.add_widget(self.create_label_pair("Totale payé", self.totale_str))
 
     def create_label_pair(self, text1, text2):
         layout = StackLayout(orientation='lr-tb', size_hint=(1, None), height=30)
+        layout_ = CustomBoxLayout(orientation='vertical', size_hint=(1, None), height=30, bg_color=(0.3, 0.3, 0.3, 1))
         layout.add_widget(Label(text=text1, size_hint=(0.4, None), height=30, bold=True, halign='right'))
         layout.add_widget(Label(text=text2, size_hint=(0.6, None), height=30, halign='right'))
-        return layout
+        layout_.add_widget(layout)
+        return layout_
 
     def create_label(self, text):
         layout = StackLayout(orientation='lr-tb', size_hint=(1, None), height=30)
+        layout_ = CustomBoxLayout(orientation='vertical', size_hint=(1, None), height=30, bg_color=(0.3, 0.3, 0.3, 1))
         layout.add_widget(Label(text=text, size_hint=(1.0, None), height=30, bold=True, halign='right'))
-        return layout
+        layout_.add_widget(layout)
+        return layout_
 
 
 class ReservationView(ScrollView):
@@ -134,7 +132,6 @@ class ReservationView(ScrollView):
             response = requests.get(url)
             response.raise_for_status()
             data = response.json()
-            print("Data retrieved:", data)  # Debugging line
 
             reservations = data.get('reservation', [])
             if reservations:
@@ -190,9 +187,18 @@ class ReservationItem(StackLayout):
 
     def create_label_pair(self, text1, text2):
         layout = StackLayout(orientation='lr-tb', size_hint=(1, None), height=30)
+        layout_ = CustomBoxLayout(orientation='vertical', size_hint=(1, None), height=30, bg_color=(0.3, 0.3, 0.3, 1))
         layout.add_widget(Label(text=text1, size_hint=(0.4, None), height=30, bold=True, halign='right'))
         layout.add_widget(Label(text=text2, size_hint=(0.6, None), height=30, halign='right'))
-        return layout
+        layout_.add_widget(layout)
+        return layout_
+
+    def create_label(self, text):
+        layout = StackLayout(orientation='vertical', size_hint=(1, None), height=30)
+        layout_ = CustomBoxLayout(orientation='vertical', size_hint=(1, None), height=30, bg_color=(0.3, 0.3, 0.3, 1))
+        layout.add_widget(Label(text=text, size_hint=(1.0, None), height=30, bold=True, halign='right'))
+        layout_.add_widget(layout)
+        return layout_
 
 
 class APIApp(App):
